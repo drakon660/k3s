b33a883bdee62680b5ad3544e3678954e886ac39
drakon660 <drakon660@gmail.com>
2020-12-18 13:30:00 +0000

    changes

--- install_dashboard.sh
+++ install_dashboard.sh
@@ -2,4 +2,6 @@
 #VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
 #echo $VERSION_KUBE_DASHBOARD
 VERSION_KUBE_DASHBOARD=v2.0.5
-sudo k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+wget https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+#sudo k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+
