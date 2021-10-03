99143ef57b054b814ca78e7d901b5699d03cba26
drakon <drakon@pop-os.localdomain>
2021-10-03 11:16:25 +0200

    install dotnet script for pop os

---
+++ install-dotnet.sh
@@ -0,0 +1,4 @@
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
+sudo apt install -y dotnet-sdk-3.1
